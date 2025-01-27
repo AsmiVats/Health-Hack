30c38e83cae0b6462db3e99eb30fcd31d8b87590
rishav-bhardwaz <rishav76000@gmail.com>
2025-01-28 01:39:47 +0530

    feat: enhance DoctorForm component to support multiple doctor entries

--- Backend/db.js
+++ Backend/db.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
-
 const MONGO_URL = process.env.MONGO_URL;
-
 const connectDB = async () => {
     try {
         await mongoose.connect(MONGO_URL, {
