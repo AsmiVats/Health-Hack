9ffa8a640ffcdd6b2bb8bbf9e158553047b96f89
rishav-bhardwaz <rishav76000@gmail.com>
2025-01-27 13:04:22 +0530

    feat: implement landing page with hero and features components

---
+++ Frontend/src/pages/Landing.jsx
@@ -0,0 +1,12 @@
+import Hero from "../components/Hero"
+import Feature from "../components/Features"
+const Landing = () => {
+  return (
+    <>
+    <Hero />
+    <Feature />
+    </>
+  )
+}
+
+export default Landing
