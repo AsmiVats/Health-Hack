bc269b9ea37689c3d9540015de7c776216d1ae84
rishav-bhardwaz <rishav76000@gmail.com>
2025-01-27 20:17:13 +0530

    feat: enhance backend database connection and add stats component to frontend

--- Frontend/src/pages/Landing.jsx
+++ Frontend/src/pages/Landing.jsx
@@ -1,10 +1,12 @@
 import Hero from "../components/Hero"
 import Feature from "../components/Features"
+import Stats from "../components/Stats"
 const Landing = () => {
   return (
     <>
     <Hero />
     <Feature />
+    <Stats />
     </>
   )
 }
